150273a19e10e2825fb7979ee3a7499725b70a43
Jon Como <jon@underground.net>
2014-09-04 00:31:28 -0700

    infinite maze generation

--- ImpossibleMaze/IMMyScene.m
+++ ImpossibleMaze/IMMyScene.m
@@ -8,41 +8,130 @@
 
 #import "IMMyScene.h"
 
+#import "MZMaze.h"
+
 @implementation IMMyScene
+{
+    SKSpriteNode *player;
+    
+    NSMutableArray *walls;
+    
+    CGPoint currentTouch;
+    CGPoint lastTouch;
+    
+    float yProgress;
+    float lastMazeY;
+    
+    BOOL createdTop;
+    int top, bottom;
+}
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.physicsWorld.gravity = CGVectorMake(0, 0);
+        self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:CGRectMake(0, 0, size.width, size.height)];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        walls = [NSMutableArray array];
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
+        player = [[SKSpriteNode alloc] initWithColor:[UIColor redColor] size:CGSizeMake(10, 10)];
+        player.position = CGPointMake(size.width/2, size.height/2);
+        [self addChild:player];
         
-        [self addChild:myLabel];
+        player.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:player.size.width/2];
+        
+        lastMazeY = 0;
+        yProgress = 0;
     }
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
+-(void)addMazeOfSize:(CGSize)size yPosition:(float)yPosition openTop:(int)openTop openBottom:(int)openBottom
+{
+    MZMaze *maze = [[MZMaze alloc] initWithSize:size];
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
+    float roomSize = self.size.width/size.width;
+    float height = size.height * roomSize;
+    
+    lastMazeY = yPosition - height/2;
+    
+    yPosition -= height;
+    
+    [maze iterateRooms:^(MZRoom *room) {
         
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+        CGPoint spawn = CGPointMake(room.x * roomSize + roomSize/2, room.y * roomSize + roomSize/2);
         
-        sprite.position = location;
+        if (!room.N && !(room.x == openTop && room.y == (int)size.height-1)){
+            [self addWallToPoint:CGPointMake(spawn.x, spawn.y + roomSize/2 + yPosition) size:CGSizeMake(roomSize, roomSize/6)];
+        }
         
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+        if (!room.W && !(room.x == 0)){
+            [self addWallToPoint:CGPointMake(spawn.x - roomSize/2, spawn.y + yPosition) size:CGSizeMake(roomSize/6, roomSize)];
+        }
         
-        [sprite runAction:[SKAction repeatActionForever:action]];
+        if (!room.S && !(room.x == openBottom && room.y == 0)){
+            [self addWallToPoint:CGPointMake(spawn.x, spawn.y - roomSize/2 + yPosition) size:CGSizeMake(roomSize, roomSize/6)];
+        }
+    }];
+}
+
+-(SKSpriteNode *)addWallToPoint:(CGPoint)point size:(CGSize)size
+{
+    SKSpriteNode *wall = [[SKSpriteNode alloc] initWithColor:[UIColor orangeColor] size:size];
+    wall.position = point;
+    wall.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:wall.size];
+    wall.physicsBody.dynamic = NO;
+    wall.alpha = 0.2;
+    [self addChild:wall];
+    
+    [walls addObject:wall];
+    
+    return wall;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    currentTouch = [[touches anyObject] locationInNode:self];
+    lastTouch = currentTouch;
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    currentTouch = [[touches anyObject] locationInNode:self];
+    
+    self.physicsWorld.gravity = CGVectorMake(currentTouch.x - lastTouch.x, currentTouch.y - lastTouch.y);
+    
+    lastTouch = currentTouch;
+}
+
+-(void)didSimulatePhysics
+{
+    float yOffset = player.position.y - self.size.height/2;
+    
+    yProgress += yOffset;
+    
+    player.position = CGPointMake(player.position.x, self.size.height/2);
+    
+    for (SKSpriteNode *wall in walls){
+        wall.position = CGPointMake(wall.position.x, wall.position.y - yOffset);
+    }
+    
+    NSLog(@"Progress: %f", yProgress);
+    
+    if (yProgress < lastMazeY){
+        CGSize size = CGSizeMake(8, 2);
         
-        [self addChild:sprite];
+        if (!createdTop){
+            createdTop = YES;
+            top = arc4random()%(int)size.width;
+            bottom = arc4random()%(int)size.width;
+        }else{
+            top = bottom;
+            bottom = arc4random()%(int)size.width;
+        }
+        
+        [self addMazeOfSize:size yPosition:lastMazeY openTop:top openBottom:bottom];
     }
 }
 
